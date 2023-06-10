50d905752ae537f2395144c0a87e375dca11e204
toaderflorin <toaderflorin@gmail.com>
2023-06-10 18:12:34 +0300

    Tweaked font.

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,55 +40,75 @@ export default function BlogIndex({ data, location }) {
           }}
         >
           <br />
-          {posts.map(post => {
-            const title = post.frontmatter.title || post.fields.slug
+          <article>
+            <p>
+              There is As great as the development experience for something like
+              Svelte is, when you start out, nothing beats the frustration of a
+              missing library. You might love functional languages and think
+              that using Elixir for your next game changing product is awesome,
+              until you find how hard it is to find experienced front-end
+              developers for it, that can jump right into the code at a moment's
+              notice. We've all seen it. Don't be dogmatic, be practical
+              instead.
+            </p>
+            <p>Don't be dogmatic, be practical instead.</p>
 
-            return (
-              <article
-                key={post.fields.slug}
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-                onClick={() => navigateToPost(post.fields.slug)}
-              >
-                <header>
-                  <h2>
-                    <span itemProp="headline">{title}</span>
-                  </h2>
-                  <div
-                    style={{
-                      fontSize: "13.5px",
-                      color: "#555",
-                      marginTop: "5px",
-                    }}
-                  >
-                    {post.frontmatter.date}
-                  </div>
-                </header>
-                <section style={{ padding: 0, marginTop: "10px" }}>
-                  <img
-                    src={`${post.fields.slug}/${post.frontmatter.icon}`}
-                    className="article-icon"
-                  />
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                  <p
-                    style={{
-                      fontWeight: "500",
-                      fontSize: "14px",
-                      color: "black",
-                    }}
-                  >
-                    Read more..
-                  </p>
-                </section>
-              </article>
-            )
-          })}
+            <div>
+            </div>
+
+            {/* <h1>READING LIST</h1> */}
+          </article>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            {posts.map(post => {
+              const title = post.frontmatter.title || post.fields.slug
+
+              return (
+                <article
+                  key={post.fields.slug}
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+                  onClick={() => navigateToPost(post.fields.slug)}
+                >
+                  <header>
+                    <h2>
+                      <span itemProp="headline">{title}</span>
+                    </h2>
+                    <div
+                      style={{
+                        fontSize: "13.5px",
+                        color: "#555",
+                        marginTop: "5px",
+                      }}
+                    >
+                      {post.frontmatter.date}
+                    </div>
+                  </header>
+                  <section style={{ padding: 0, marginTop: "10px" }}>
+                    <img
+                      src={`${post.fields.slug}/${post.frontmatter.icon}`}
+                      className="article-icon"
+                    />
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                    <p
+                      style={{
+                        fontWeight: "500",
+                        fontSize: "14px",
+                        color: "black",
+                      }}
+                    >
+                      Read more..
+                    </p>
+                  </section>
+                </article>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
